23bafaf7b945992ffd933d48036bd1ad940b820e
Ian Andrade Moreira <ianmoreira80@gmail.com>
2020-04-03 23:29:23 -0300

    feature: hello world

--- src/main/java/com/devmobil/Vendas/VendasApplication.java
+++ src/main/java/com/devmobil/Vendas/VendasApplication.java
@@ -2,10 +2,17 @@ package com.devmobil.Vendas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
 
+	@GetMapping("/")
+	public String helloWord() {
+		return "helloWord";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
